d2fec8fcb62e2bb5fc8d629841a17f82155a7542
Edouard Lopez <dev+git@edouard-lopez.com>
2013-08-30 17:35:06 +0200

    feat(PHP): main script

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
   $root = $_SERVER['DOCUMENT_ROOT'];
    $user = 'ed'; // needed for quick-menu
-   
+
   require_once 'cv.phpc';
 //   require_once 'abbr.php';
 
-    $me = new cv();
+    $me = new CV();
 //   require_once 'user.phpc';
     $me->setFirstName('Édouard');
     $me->setLastName('Lopez');
@@ -14,14 +14,13 @@
     $fullName = $me->getFullName();
     $me->setUserName('ed');
     $me->setLocaleEnvFromUserName();
-    
+
     $me->setFacetedSearch( array(
         'cog' => $me->_cv('Cognition, Teaching'),
-        'dev'    =>  $me->_cv('Programming'), 
+        'dev'    =>  $me->_cv('Programming'),
         'i18n'   =>  $me->_cv('Languages, Abroad'),
-        'ux'      =>  sprintf('%s, %s', $me->_cv('Accessibility'), sprintf('<abbr title="%s">UX</abbr>', $me->_cv('User eXperience'))), 
+        'ux'      =>  sprintf('%s, %s', $me->_cv('Accessibility'), sprintf('<abbr title="%s">UX</abbr>', $me->_cv('User eXperience'))),
         'web'   =>  $me->_cv('Web'),
     ));
   require_once 'abbr.php';
     $me->displayBodyElement();
-?>
