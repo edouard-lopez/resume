613ab6663d6b6be13b2008352548956c3ebfebbf
Edouard Lopez <dev+git@edouard-lopez.com>
2013-09-02 15:40:47 +0200

    feat(navbar): add navbar and search to UI

---
+++ app/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <div class="dropdown">
+            <a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown">
+               <span class="glyphicon glyphicon-home"></span>
+               <b class="caret"></b>
+           </a>
+           <ul class="dropdown-menu">
+            <li><a href="/cv">CV</a></li>
+            <li><a href="/portfolio">portfolio</a></li>
+            <li><a href="/fac">fac</a></li>
+            <li class="divider"></li>
+            <li class="nav-header">Nav header</li>
+            <li><a href="/labo">labo</a></li>
+            <li><a href="/wiki">wiki</a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Collect the nav links, forms, and other content for toggling -->
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <div id="navbar-nav">
+        <ol class="breadcrumb">
+            <li>⌘</li>
+            <li><a href="#">Édouard&nbsp;Lopez</a></li>
+            <li class="active">CV</li>
+        </ol>
+    </div>
+    <form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+</div><!-- /.navbar-collapse -->
+</nav>
