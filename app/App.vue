630de1f32ce4328381c9f1abbbc004f8f4a17f89
Édouard Lopez <dev+git@edouard-lopez.com>
2016-01-11 08:21:22 +0100

    display <resume> layout

--- app/App.vue
+++ app/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <img class="logo" src="./assets/logo.png">
     <hello></hello>
+    <resume></resume>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Resume from './components/Resume'
 
 export default {
   components: {
-    Hello
+    Hello,
+    Resume
   }
 }
 </script>
