82b1bfc5a5b507ed1e0b8f485b6f06bdd33cc41d
Édouard Lopez <dev+git@edouard-lopez.com>
2016-01-27 01:53:35 +0100

    add Tags.vue file

---
+++ app/components/Tags.vue
@@ -0,0 +1,16 @@
+<aside class="tags">
+  <ol v-for="{{}}">
+    {{resume}}
+
+  </ol>
+</aside>
+
+<script>
+  export default {
+    data () {
+      return {
+        // initial state
+      }
+    }
+  }
+</script>
