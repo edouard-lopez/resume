91b1e131db8e9b2823e7849cf7dc66cbcea86725
Édouard Lopez <contact@edouard-lopez.com>
2023-09-13 00:31:49 +0200

    feat: add avatar from github and twitter link

--- src/resumes/cool.vue
+++ src/resumes/cool.vue
@@ -32,13 +32,8 @@
           </div>
 
           <div class="section-content-grid">
-            <a
-              v-for="(skill, index) in person.skills"
-              class="grid-item"
-              :key="index"
-              :class="{ link: skill.url !== undefined }"
-              :href="skill.url"
-            >
+            <a v-for="(skill, index) in person.skills" class="grid-item" :key="index"
+              :class="{ link: skill.url !== undefined }" :href="skill.url">
               <span class="squarred-grid-item">
                 {{ skill.name }}
               </span>
@@ -52,55 +47,36 @@
           </div>
 
           <div class="section-content section-content--plain">
-            <div class="section-link">
-              <i class="section-link__icon material-icons">business</i
-              >{{ person.contact.street }}
+            <div v-if="person.contact.street" class="section-link">
+              <i class="section-link__icon material-icons">business</i>{{ person.contact.street }}
             </div>
 
             <a class="section-link link" :href="contactLinks.email">
-              <i class="section-link__icon material-icons">mail</i
-              >{{ person.contact.email }}
+              <i class="section-link__icon material-icons">mail</i>{{ person.contact.email }}
             </a>
 
             <div class="section-link">
-              <i class="section-link__icon material-icons">phone</i
-              >{{ person.contact.phone }}
+              <i class="section-link__icon material-icons">phone</i>{{ person.contact.phone }}
             </div>
 
-            <a
-              v-if="person.contact.website"
-              class="section-link link"
-              :href="person.contact.website"
-            >
-              <i class="section-link__icon fa fa-globe"></i
-              >{{ person.contact.website }}
+            <a v-if="person.contact.website" class="section-link link" :href="person.contact.website">
+              <i class="section-link__icon fa fa-globe"></i>{{ person.contact.website }}
             </a>
 
-            <a
-              v-if="person.contact.linkedin"
-              class="section-link link"
-              :href="contactLinks.linkedin"
-            >
-              <i class="section-link__icon fa fa-linkedin"></i
-              >{{ person.contact.linkedin }}
+            <a v-if="person.contact.linkedin" class="section-link link" :href="contactLinks.linkedin">
+              <i class="section-link__icon fa fa-linkedin"></i>{{ person.contact.linkedin }}
             </a>
 
-            <a
-              v-if="person.contact.github"
-              class="section-link link"
-              :href="contactLinks.github"
-            >
-              <i class="section-link__icon fa fa-github"></i
-              >{{ person.contact.github }}
+            <a v-if="person.contact.github" class="section-link link" :href="contactLinks.github">
+              <i class="section-link__icon fa fa-github"></i>{{ person.contact.github }}
             </a>
 
-            <a
-              v-if="person.contact.medium"
-              class="section-link link"
-              :href="contactLinks.medium"
-            >
-              <i class="section-link__icon fa fa-medium"></i
-              >{{ person.contact.medium }}
+            <a v-if="person.contact.twitter" class="section-link link" :href="contactLinks.twitter">
+              <i class="section-link__icon fa fa-twitter"></i>{{ person.contact.twitter }}
+            </a>
+
+            <a v-if="person.contact.medium" class="section-link link" :href="contactLinks.medium">
+              <i class="section-link__icon fa fa-medium"></i>{{ person.contact.medium }}
             </a>
           </div>
         </div>
@@ -109,25 +85,20 @@
       <div class="content__right">
         <div class="section">
           <div class="section-headline">
-            <i class="section-headline__icon material-icons">location_city</i
-            >{{ lang.ngo }}
+            <i class="section-headline__icon material-icons">location_city</i>{{ lang.ngo }}
           </div>
 
           <div class="section-content">
-            <a
-              v-for="(ngo, index) in person.ngo"
-              :key="index"
-              class="section-content__item"
-              :class="{ link: ngo.website !== undefined }"
-              :href="ngo.website"
-            >
+            <a v-for="(ngo, index) in person.ngo" :key="index" class="section-content__item"
+              :class="{ link: ngo.website !== undefined }" :href="ngo.website">
               <span class="section-content__header">{{ ngo.position }}</span>
               <span class="section-content__subheader">
                 {{ ngo.company }}
                 <span class="section-content__plain">{{ ngo.location }}</span>
               </span>
 
-              <div class="section-content__text">{{ ngo.timeperiod }}</div>
+              <div class="section-content__text__header">{{ ngo.name }}</div>
+              <div class="section-content__text--light">{{ ngo.timeperiod }}</div>
               <span class="section-content__text--light">{{
                 ngo.description
               }}</span>
@@ -135,18 +106,12 @@
           </div>
 
           <div class="section-headline">
-            <i class="section-headline__icon material-icons">work</i
-            >{{ lang.experience }}
+            <i class="section-headline__icon material-icons">work</i>{{ lang.experience }}
           </div>
 
           <div class="section-content">
-            <a
-              v-for="(experience, index) in person.experience"
-              :key="index"
-              class="section-content__item"
-              :class="{ link: experience.website !== undefined }"
-              :href="experience.website"
-            >
+            <a v-for="(experience, index) in person.experience" :key="index" class="section-content__item"
+              :class="{ link: experience.website !== undefined }" :href="experience.website">
               <span class="section-content__header">{{
                 experience.position
               }}</span>
@@ -169,18 +134,12 @@
 
         <div class="section">
           <div class="section-headline">
-            <i class="section-headline__icon material-icons">school</i
-            >{{ lang.education }}
+            <i class="section-headline__icon material-icons">school</i>{{ lang.education }}
           </div>
 
           <div class="section-content">
-            <a
-              v-for="(education, index) in person.education"
-              class="section-content__item"
-              :key="index"
-              :class="{ link: education.website !== undefined }"
-              :href="education.website"
-            >
+            <a v-for="(education, index) in person.education" class="section-content__item" :key="index"
+              :class="{ link: education.website !== undefined }" :href="education.website">
               <span class="section-content__header">
                 {{ education.school }}
               </span>
@@ -199,18 +158,12 @@
 
         <div v-if="person.projects" class="section">
           <div class="section-headline">
-            <i class="section-headline__icon material-icons">code</i
-            >{{ lang.projects }}
+            <i class="section-headline__icon material-icons">code</i>{{ lang.projects }}
           </div>
 
           <div class="section-content-grid">
-            <a
-              v-for="(project, index) in person.projects"
-              :key="index"
-              class="section-content__item-grid"
-              :class="{ link: project.url !== undefined }"
-              :href="project.url"
-            >
+            <a v-for="(project, index) in person.projects" :key="index" class="section-content__item-grid"
+              :class="{ link: project.url !== undefined }" :href="project.url">
               <span class="section-content__header"> {{ project.name }} </span>
               <span class="section-content__subheader">{{
                 project.platform
@@ -224,28 +177,19 @@
 
         <div v-if="person.contributions" class="section">
           <div class="section-headline">
-            <i class="section-headline__icon fa fa-heart"></i
-            >{{ lang.contributions }}
+            <i class="section-headline__icon fa fa-heart"></i>{{ lang.contributions }}
           </div>
 
           <div class="section-content-grid">
-            <a
-              v-for="(contribution, index) in person.contributions"
-              class="section-content__item-grid"
-              :key="index"
-              :class="{ link: contribution.url !== undefined }"
-              :href="contribution.url"
-            >
+            <a v-for="(contribution, index) in person.contributions" class="section-content__item-grid" :key="index"
+              :class="{ link: contribution.url !== undefined }" :href="contribution.url">
               <span class="section-content__header">
                 {{ contribution.name }}
               </span>
               <span class="section-content__text">
                 {{ contribution.description }}
               </span>
-              <span
-                class="section-content__text--light"
-                style="word-break: break-all"
-              >
+              <span class="section-content__text--light" style="word-break: break-all">
                 {{ contribution.url }}
               </span>
             </a>
@@ -254,198 +198,202 @@
       </div>
     </div>
 
-    <img class="picture" />
+    <img class="picture" :src="person.contact.avatar" />
   </div>
 </template>
 
 <script>
-  import Vue from 'vue';
-  import { getVueOptions } from './options';
+import Vue from 'vue';
+import { getVueOptions } from './options';
 
-  const name = 'cool';
+const name = 'cool';
 
-  export default Vue.component(name, getVueOptions(name));
+export default Vue.component(name, getVueOptions(name));
 </script>
 
 <style lang="less" scoped>
-  @accent-color: #34495e;
-  @banner-color: #42b883;
-  @banner-height: 120px;
-  @picture-size: 120px;
-  @picture-offset: 35px;
-  @base-padding: 30px;
-  @left-column-width: 240px;
+@accent-color: #34495e;
+@banner-color: #42b883;
+@banner-height: 120px;
+@picture-size: 120px;
+@picture-offset: 35px;
+@base-padding: 30px;
+@left-column-width: 240px;
 
-  .link {
+.link {
+  color: inherit;
+  cursor: pointer;
+  text-decoration-line: none;
+
+  &:visited {
     color: inherit;
-    cursor: pointer;
-    text-decoration-line: none;
-
-    &:visited {
-      color: inherit;
-    }
   }
+}
 
-  .resume {
-    position: relative;
-    font-family: 'Roboto' !important;
-    font-size: 0.9em;
-  }
+.resume {
+  position: relative;
+  font-family: 'Roboto' !important;
+  font-size: 0.9em;
+}
 
-  .picture {
-    position: absolute;
-    top: @banner-height - @picture-offset;
-    left: @left-column-width + @base-padding * 2 - @picture-size / 2;
-    height: @picture-size;
-    width: @picture-size;
-    border-radius: 50%;
-    border: 5px solid @accent-color;
-    content: url('../../resume/id.jpg');
-    z-index: 2;
-  }
+.picture {
+  position: absolute;
+  top: @banner-height - @picture-offset;
+  left: @left-column-width + @base-padding * 2 - @picture-size / 2;
+  height: @picture-size;
+  width: @picture-size;
+  border-radius: 50%;
+  border: 5px solid @accent-color;
+  // content: url('../../resume/id.jpg');
+  z-index: 2;
+}
 
-  .banner {
-    width: calc(100% - @base-padding * 2);
-    height: @banner-height;
-    padding: @base-padding;
-    background-color: @banner-color;
-    /*
+.banner {
+  width: calc(100% - @base-padding * 2);
+  height: @banner-height;
+  padding: @base-padding;
+  background-color: @banner-color;
+  /*
     background-image: url('../../resume/banner.png');
     background-repeat: no-repeat;
     background-size: cover;
   */
-    color: white;
+  color: white;
 
-    &__fullname {
-      font-size: 32px;
-    }
-
-    &__position {
-      font-size: 16px;
-    }
-
-    &__location {
-      font-size: 12px;
-    }
+  &__fullname {
+    font-size: 32px;
   }
 
-  .content {
-    display: flex;
-    width: 100%;
+  &__position {
+    font-size: 16px;
+  }
+
+  &__location {
+    font-size: 12px;
+  }
+}
+
+.content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  &__left,
+  &__right {
     height: 100%;
-
-    &__left,
-    &__right {
-      height: 100%;
-      padding: @base-padding;
-    }
-
-    &__left {
-      width: @left-column-width;
-      color: rgba(255, 255, 255, 0.59);
-      background-color: @accent-color;
-
-      .section-headline {
-        color: white;
-      }
-    }
-
-    &__right {
-      flex: 1;
-    }
+    padding: @base-padding;
   }
 
-  .section {
-    margin: 20px 0;
-  }
+  &__left {
+    width: @left-column-width;
+    color: rgba(255, 255, 255, 0.59);
+    background-color: @accent-color;
 
-  .section-link,
-  .section-headline {
-    display: flex !important;
-    align-items: center;
-    color: @accent-color;
-    display: inline-block;
-    font-size: 1.2em;
-    margin: 8px 0;
-
-    &__icon {
-      margin-right: 8px;
-      font-size: 1.4em;
-    }
-  }
-
-  .section-link {
-    font-size: 1.1em;
-    color: rgba(255, 255, 255, 0.59) !important;
-
-    &__icon {
+    .section-headline {
       color: white;
     }
   }
 
-  .section-content {
-    margin-top: 5px;
-    padding-left: 32px;
-    font-size: 14px;
-
-    &__item {
-      display: block;
-      margin-bottom: 5px;
-    }
-
-    &__header {
-      display: block;
-      font-size: 1.1em;
-      font-weight: 500;
-    }
-
-    &__subheader {
-      display: block;
-      font-weight: 400;
-    }
-
-    &__plain,
-    &__text {
-      display: block;
-      font-size: 12px;
-
-      &--light {
-        font-size: 12px;
-      }
-    }
-
-    &__plain {
-      display: inline;
-      font-weight: 300;
-    }
-
-    &__item-grid {
-      flex: 1 1 0;
-      margin-bottom: 5px;
-      padding-right: 5px;
-    }
-
-    &--plain {
-      padding: 0;
-    }
+  &__right {
+    flex: 1;
   }
+}
 
-  .section-content-grid {
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 5px;
+.section {
+  margin: 20px 0;
+}
+
+.section-link,
+.section-headline {
+  display: flex !important;
+  align-items: center;
+  color: @accent-color;
+  display: inline-block;
+  font-size: 1.2em;
+  margin: 8px 0;
+
+  &__icon {
+    margin-right: 8px;
+    font-size: 1.4em;
+  }
+}
+
+.section-link {
+  font-size: 1.1em;
+  color: rgba(255, 255, 255, 0.59) !important;
+
+  &__icon {
+    color: white;
+  }
+}
+
+.section-content {
+  margin-top: 5px;
+  padding-left: 32px;
+  font-size: 14px;
+
+  &__item {
+    display: block;
     margin-bottom: 5px;
   }
 
-  .grid-item {
+  &__header {
+    display: block;
+    font-size: 1.1em;
+    font-weight: 500;
+  }
+
+  &__subheader {
+    display: block;
+    font-weight: 400;
+  }
+
+  &__plain,
+  &__text {
+    display: block;
+    font-size: 12px;
+
+    &--bold {
+      font-weight: bold;
+    }
+
+    &--light {
+      font-size: 12px;
+    }
+  }
+
+  &__plain {
+    display: inline;
+    font-weight: 300;
+  }
+
+  &__item-grid {
+    flex: 1 1 0;
+    margin-bottom: 5px;
     padding-right: 5px;
   }
 
-  .squarred-grid-item {
-    display: block;
-    border: 1px solid white;
-    color: white;
-    margin-top: 5px;
-    padding: 5px;
+  &--plain {
+    padding: 0;
   }
+}
+
+.section-content-grid {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.grid-item {
+  padding-right: 5px;
+}
+
+.squarred-grid-item {
+  display: block;
+  border: 1px solid white;
+  color: white;
+  margin-top: 5px;
+  padding: 5px;
+}
 </style>
